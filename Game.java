055ec44ff05ccb4d6c8b9df3a946e3afc1d62236
Giorgos Malandrakis <93475472+giormala@users.noreply.github.com>
2021-11-07 16:19:07 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
 public class Game{
     public static void main(String[] args) {
         System.out.println("Hello git world!!!");
+        System.out.println("Kalispera kai kali vradia");
     }
 }
