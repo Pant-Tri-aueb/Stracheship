cefffd4d603fd4cde2344390d3245f35472821f9
sergiloupa <93870667+sergiloupa@users.noreply.github.com>
2021-11-07 16:20:09 +0200

    Update SecondGame.java

--- SecondGame.java
+++ SecondGame.java
@@ -1,6 +1,7 @@
 public class SecondGame {
     public static void main(String[] args) {
         System.out.println("where this world goes???");
+        System.out.println(" exw kokooo sto trapezi");
     }
 }
-swstossss
+  
