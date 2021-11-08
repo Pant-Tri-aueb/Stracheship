63f4db7a0330ff50231e819013d2593f4d82979c
AkisPanagiotopoulos <akispan7@gmail.com>
2021-11-09 00:45:35 +0200

    DSADASDA

--- Game.java
+++ Game.java
@@ -4,22 +4,5 @@ public class Game{
         System.out.println("Kalispera kai kali vradia");
         System.out.println("ooooooof");
         System.out.println("Gamw ton Apollwna");
-<<<<<<< Updated upstream
-        System.out.println("Gamw ton Apollwna kai pali");
-        System.out.println("Pame triantafylle Arxhge!");
-<<<<<<< HEAD
-        System.out.println("ela mori omadara");
-        System.out.println("Sorry re");
-        
-=======
-=======
-        System.out.println("Gamw ton Apollwna");
-        System.out.println("Gamw ton Apollwna");
-        System.out.println("Gamw ton Apollwna");
-        System.out.println("Gamw ton Apollwna");
-        System.out.println("Gamw ton Apollwna");
-        System.out.println("Gamw tin AEK");
->>>>>>> Stashed changes
->>>>>>> 442c459aeffdad602f44992759dd15122dab8a64
     }
 }
