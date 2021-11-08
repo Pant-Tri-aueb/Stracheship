c34e1104c1a58492aada07ad72dceb55dca37195
Giorgos Malandrakis <geomalandrakis@yahoo.gr>
2021-11-08 21:54:48 +0200

    etsi gia plaka

--- SecondGame.java
+++ SecondGame.java
@@ -1,6 +1,6 @@
 public class SecondGame {
     public static void main(String[] args) {
-        System.out.println("where this world goes???");
+        System.out.println("where this world goes haha???");
     }
 }
   
