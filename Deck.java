d1613572ac6c93da67cfa6c4e9b9e8a5a4a60c43
AkisPanagiotopoulos <akispan7@gmail.com>
2021-11-11 01:18:14 +0200

    Update Deck Akaros

--- Deck.java
+++ Deck.java
@@ -1,12 +1,12 @@
-public class Deck {
+public class Deck {  //Mia malakia pou ekana ston eleuthero moy xrono //
 
     private String[][] deck_arr = new String[10][10];
 
-    public Deck(){
+    public Deck() {  // arxikopoiei pinaka 2x2 pou deixnei to deck tou paixth me "O" tis theseis tou Deck//
         DeckInitialization();
     }
 
-    public void DeckInitialization() {
+    public void DeckInitialization() { // Methodo arxikopoihshs tou Deck //
 
         for(int i=0 ; i<10 ; i++) {
             for(int j=0 ; j<10 ; j++) {
@@ -16,6 +16,10 @@ public class Deck {
     }
 
     public void shipPlacement(int x,int y,int size,String direction) {
+        // Topothetisi ploiou opoy x kai y oi syntetagmenes enos simeioy,
+        // size to megethos toy ploioy kai direction to ean mpainei to ploio katheta h orizontia
+        // Ayth h methodo thelei kai ena check gia tin periptosh topothethshs ploiou pou den xoraei sto Deck//
+
         int i; 
         if (direction.equals("RIGHT")) {
              for (i = y ; i < size + 1 ; i++){
@@ -28,7 +32,7 @@ public class Deck {
          }
     }
 
-    public void PrintArray(){
+    public void PrintArray(){  // Aplh voithitikh methodo gia Print tou Deck//
         for(int i=0 ; i<10 ; i++) {
             for(int j=0 ; j<10 ; j++) {
                 System.out.print(this.deck_arr[i][j]);
@@ -37,6 +41,8 @@ public class Deck {
         }
         
     }
+
+    
 
 
 
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main { // Mia apli main gia na trexei h Deck.java //
     public static void main(String[] args) {
         Deck a = new Deck();
         a.PrintArray();
