3cdab84a5804042afdb1ab7386a82944ca674d8e
AkisPanagiotopoulos <akispan7@gmail.com>
2021-11-19 02:44:33 +0200

    pull

--- Deck.java
+++ Deck.java
@@ -6,6 +6,34 @@ public class Deck {  //Mia malakia pou ekana ston eleuthero moy xrono //
         DeckInitialization();
     }
 
+    public void shipPlacement(int x,int y,int size,String direction) {
+        // Topothetisi ploiou opoy x kai y oi syntetagmenes enos simeioy,
+        // size to megethos toy ploioy kai direction to ean mpainei to ploio katheta h orizontia
+        // Ayth h methodo thelei kai ena check gia tin periptosh topothethshs ploiou pou den xoraei sto Deck//
+        boolean flag1;
+        boolean flag2;
+        int i;
+        final String RIGHT = "RIGHT";
+        final String DOWN = "DOWN";
+        if (direction.equals(RIGHT)) {
+            flag1 = ShipOutOfDeckCheck(x, y, size, RIGHT);
+            flag2 = ShipOnShipCheck(x, y, size, RIGHT);
+            if ((flag1 != false) && (flag2 != false)) {
+            	for (i = y ; i < size + y ; i++){
+            		 this.deck_arr[x-1][i-1] = "S";
+            	}
+            }
+        } else if (direction.equals(DOWN)) {
+            flag1 = ShipOutOfDeckCheck(x, y, size, DOWN);
+            flag2 = ShipOnShipCheck(x, y, size, DOWN);
+            if ((flag1 != false) && (flag2 != false)) {
+            	for (i = x ; i < size + x ; i++){
+            		this.deck_arr[i-1][y-1] = "S";
+            	}
+            }
+        }
+    }
+
     public void DeckInitialization() { // Methodo arxikopoihshs tou Deck //
 
         for(int i=0 ; i<10 ; i++) {
@@ -13,40 +41,6 @@ public class Deck {  //Mia malakia pou ekana ston eleuthero moy xrono //
                 this.deck_arr[i][j] = "O";
             }
         }
-    }
-
-    public void shipPlacement(int x,int y,int size,String direction) {
-        // Topothetisi ploiou opoy x kai y oi syntetagmenes enos simeioy,
-        // size to megethos toy ploioy kai direction to ean mpainei to ploio katheta h orizontia
-        // Ayth h methodo thelei kai ena check gia tin periptosh topothethshs ploiou pou den xoraei sto Deck//
-
-        int i; 
-        boolean checkship = true;
-        if (direction.equals("RIGHT")) {
-             for (i = y ; i < size + y ; i++){
-                 if (this.deck_arr[x-1][i-1] == "S") {
-                	 checkship = false;
-                	 System.out.println("Στην γραμμή " + x + " και σειρά " + i + " υπάρχει άλλο πλοίο");
-                 }
-             }
-             if (checkship == true) {
-            	 for (i = y ; i < size + y ; i++){
-            		 this.deck_arr[x-1][i-1] = "S";
-            	 }
-             }
-         } else if (direction.equals("DOWN")) {
-        	 for (i = x ; i < size + x ; i++){
-                 if (this.deck_arr[i-1][y-1] == "S") {
-                	 checkship = false;
-                	 System.out.println("Στην γραμμή " + x + " και σειρά " + i + " υπάρχει άλλο πλοίο");
-                 }
-             }
-             if (checkship == true) {
-            	 for (i = x ; i < size + x ; i++){
-            		 this.deck_arr[i-1][y-1] = "S";
-            	 }
-             }
-         }
     }
 
     public void PrintArray(){  // Aplh voithitikh methodo gia Print tou Deck//
@@ -58,5 +52,42 @@ public class Deck {  //Mia malakia pou ekana ston eleuthero moy xrono //
             }
         }
         
+    }
+
+    // Methodo elegxou topothetishs ploiou ston pinaka //
+    public boolean ShipOutOfDeckCheck(int x, int y, int size, String direction) {
+        boolean checkship = true;
+        if ((x > 10) || (y > 10)) {
+            System.out.println("Άκυρη τοποθέτηση πλοίου.\nΟι συντεταγμένες του πλοίου δεν υπάρχουν στο ταμπλό!");
+            checkship = false;
+            return checkship;
+        }
+        if (((direction.equals("RIGHT")) && (y + size - 1 > 10)) || ((direction.equals("DOWN")) && (x + size - 1 > 10))) {
+            System.out.println("Άκυρη τοποθέτηση πλοίου.\nΤο πλοίο βγαίνει εκτός ταμπλό παιχνιδιού!");
+            checkship = false;
+            return checkship;
+        }
+        return checkship;
+    }
+
+    public boolean ShipOnShipCheck(int x, int y, int size, String direction) {
+        int i;
+        boolean checkship = true;
+        if (direction.equals("RIGHT")) {
+            for (i = y ; i < size + y ; i++){
+                if (this.deck_arr[x-1][i-1] == "S") {
+                	 checkship = false;
+                	 System.out.println("Στην γραμμή " + x + " και σειρά " + i + " υπάρχει άλλο πλοίο");
+                }
+            }
+        } else if (direction.equals("DOWN")) {
+            for (i = x ; i < size + x ; i++){
+                if (this.deck_arr[i-1][y-1] == "S") {
+                    checkship = false;
+                    System.out.println("Στην γραμμή " + x + " και σειρά " + i + " υπάρχει άλλο πλοίο");
+                }
+            }
+        }
+        return checkship;
     }
 }
