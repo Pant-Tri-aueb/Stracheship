11668d7f827ea895a196626b3ead624861410f33
filf13 <94014907+filf13@users.noreply.github.com>
2021-11-30 22:24:50 +0200

    Create Artemis.java

---
+++ Artemis.java
@@ -0,0 +1,12 @@
+public class Artemis extends Godsj {
+
+	private int times = 0;
+	Ship2 ship = new Ship2();
+	public void Protection ( int times , Ship2 ship.getName , Ship2 ship.getTolerance) {
+		if (times == 0 || times ==1) {
+			
+		}
+		
+	}
+}
+
