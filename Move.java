ca19509cf93d1f85c51acdab292919a8cf1264e6
Giorgos Malandrakis <geomalandrakis@yahoo.gr>
2021-12-01 00:38:27 +0200

    προσθήκη κλάσης Move

---
+++ Move.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Move {
+	
+	Deck boat = new Deck();
+	
+	int i;
+	int x;
+	int y;
+	int size;
+	String direction;
+	
+	public void moveRight(int x, int y, int size, String direction) {
+		//x = boat.ploia.get(i).getX();
+		//y = boat.ploia.get(i).getSize();
+		//size = boat.ploia.get(i).getSize();
+		//direction = boat.ploia.get(i).getDirection();
+		
+		if (direction.equals("RIGHT") ) {
+			if (y + size - 1 <= 9) {
+				MenuInterface.Deck1.deck_arr[x][y] = "O";
+				MenuInterface.Deck1.deck_arr[x][y+size] = "S";
+				//boat.ploia.get(i).setY(y+1);
+			} else {
+				System.out.println("Δεν επιτρέπεται!");
+			}
+			
+		}
+	}
+	
+	public void moveLeft(int i) {
+		i = i-1;
+		x = boat.ploia.get(i).getX();
+		y = boat.ploia.get(i).getY();
+		size = boat.ploia.get(i).getSize();
+		direction = boat.ploia.get(i).getDirection();
+		
+		if (direction.equals("RIGHT") ) {
+			if (y > 1) {
+				MenuInterface.Deck1.deck_arr[x][y+size-1] = "O";
+				MenuInterface.Deck1.deck_arr[x][y-1] = "S";
+				boat.ploia.get(i).setY(y-1);
+			} else {
+				System.out.println("Δεν επιτρέπεται!");
+			}
+			
+		}
+	}
+	
+	public void moveUp(int i) {
+		i = i-1;
+		x = boat.ploia.get(i).getX();
+		y = boat.ploia.get(i).getY();
+		size = boat.ploia.get(i).getSize();
+		direction = boat.ploia.get(i).getDirection();
+		
+		if (direction.equals("DOWN") ) {
+			if (x > 1) {
+				MenuInterface.Deck1.deck_arr[x+size-1][y] = "O";
+				MenuInterface.Deck1.deck_arr[x-1][y] = "S";
+				boat.ploia.get(i).setX(x-1);
+			} else {
+				System.out.println("Δεν επιτρέπεται!");
+			}
+			
+		}
+	}
+	
+	public void moveDown(int i) {
+		i = i-1;
+		x = boat.ploia.get(i).getX();
+		y = boat.ploia.get(i).getY();
+		size = boat.ploia.get(i).getSize();
+		direction = boat.ploia.get(i).getDirection();
+		
+		if (direction.equals("DOWN") ) {
+			if (x + size - 1 <= 9) {
+				MenuInterface.Deck1.deck_arr[x][y] = "O";
+				MenuInterface.Deck1.deck_arr[x+size][y] = "S";
+				boat.ploia.get(i).setX(x+1);
+			} else {
+				System.out.println("Δεν επιτρέπεται!");
+			}
+			
+		}
+	}
+}
--- Ship2.java
+++ Ship2.java
@@ -1,19 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ship2 {
 
     private char name;
     private int capacity;
     private int tolerance;
+    private int x;
+    private int y;
+    private int size;
+    private String direction;
 
-    public Ship2(char name, int capacity, int tolerance) {
-        this.name = name;
-        this.capacity = capacity;
-        this.tolerance = tolerance;
+    public Ship2(int x, int y, int size, String direction) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.direction = direction;
     }
 
     public int getCapacity() {
         return capacity;
     }
+    
+    public int getX() {
+        return x;
+    }
 
+    public int getY() {
+        return y;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+    
+    public String getDirection() {
+        return direction;
+    }
+    
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
