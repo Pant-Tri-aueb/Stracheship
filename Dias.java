6ec0d157bc3de9e17516beb6f851018e9badb3d3
filf13 <94014907+filf13@users.noreply.github.com>
2021-11-30 22:24:11 +0200

    Create Dias.java

---
+++ Dias.java
@@ -0,0 +1,20 @@
+public class Dias extends Godsj {
+
+	Ship2 ship = new Ship2();
+	
+	public Ship2 Strike(Ship2 ship.getName , Ship2 ship.getTolerance ) {
+		if (ship.getTolerance() > 0 && ship.getName == "a" || ship.getName == "b") {
+			ship.setTolerance() == 0;
+		}else {
+			ship.setTolerance() == 1;
+		}
+        return ship.getTolerance();	
+	}
+
+	public Ship2 Resurrection(Ship2 ship.getTolerance) {
+		if (ship.getTolerance() > 0) {
+			ship.getTolerance() = 3;
+			return ship.getTolerance();
+		}
+	}
+}
