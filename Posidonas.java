4a905d4e932e19001ceb40b71013c6b8bbd9356e
AkisPanagiotopoulos <akispan7@gmail.com>
2021-12-01 20:16:39 +0200

    Poseidon αρχικη προσπαθεια Ακαρος

--- Posidonas.java
+++ Posidonas.java
@@ -1,6 +1,28 @@
-public class Posidonas extends Godsj {
+import java.util.Scanner;
 
-	public void attack {
+public class Posidonas {
+
+	private int poseidonDefenceTries = 5;
+
+	public boolean doDefence(){
+		Scanner defencequestionScanner = new Scanner(System.in);
+		System.out.println("Έχεις " + poseidonDefenceTries + " διαθέσιμες άμυνες του Ποσειδώνα, θέλεις να τις χρησιμοποιήσεις;\nY/N?");
+		String  answer = defencequestionScanner.nextLine();
+		boolean defence;
+		if (answer == "Y") {
+			defence = true;
+		} else {
+			defence = false;
+		}
+		return defence;
+	}
+
+	public void poseidonDefence() {
 		
+		if (doDefence() == true) {
+			
+			poseidonDefenceTries-- ;
+		}
+
 	}
 }
