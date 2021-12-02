753e41ad04610730ae1983fca2afacc06ccfeafb
GrammatikakisDimitris <92916988+GrammatikakisDimitris@users.noreply.github.com>
2021-12-02 19:15:23 +0200

    Update Move.java

--- Move.java
+++ Move.java
@@ -3,26 +3,26 @@ import java.util.List;
 
 public class Move {
 	
-	Deck boat = new Deck();
-	//
+	//Deck Ship2.shipsList = new Deck();
+	
 	int i;
 	int x;
 	int y;
 	int size;
-	String direction;
+	String direction; 
 	
 	public void moveRight(int i) {
-		i=i-1;
-		x = boat.pl11.getX();
-		y = boat.pl11.getY();
-		size = boat.pl11.getSize();
-		direction = boat.pl11.getDirection();
+		i = i - 1;
+		x = Ship2.shipsList.get(i).getX();
+		y = Ship2.shipsList.get(i).getY();
+		size = Ship2.shipsList.get(i).getSize();
+		direction = Ship2.shipsList.get(i).getDirection();
 		
 		if (direction.equals("RIGHT") ) {
 			if (y + size - 1 <= 9) {
 				MenuInterface.Deck1.deck_arr[x][y] = "O";
 				MenuInterface.Deck1.deck_arr[x][y+size] = "S";
-				boat.pl11.setY(y+1);
+				Ship2.shipsList.get(i).setY(y+1);
 			} else {
 				System.out.println("Δεν επιτρέπεται!");
 			}
@@ -31,17 +31,17 @@ public class Move {
 	}
 	
 	public void moveLeft(int i) {
-		i = i-1;
-		x = boat.ploia.get(i).getX();
-		y = boat.ploia.get(i).getY();
-		size = boat.ploia.get(i).getSize();
-		direction = boat.ploia.get(i).getDirection();
+		i = i - 1;
+		x = Ship2.shipsList.get(i).getX();
+		y = Ship2.shipsList.get(i).getY();
+		size = Ship2.shipsList.get(i).getSize();
+		direction = Ship2.shipsList.get(i).getDirection();
 		
 		if (direction.equals("RIGHT") ) {
 			if (y > 1) {
 				MenuInterface.Deck1.deck_arr[x][y+size-1] = "O";
 				MenuInterface.Deck1.deck_arr[x][y-1] = "S";
-				boat.ploia.get(i).setY(y-1);
+				Ship2.shipsList.get(i).setY(y-1);
 			} else {
 				System.out.println("Δεν επιτρέπεται!");
 			}
@@ -51,16 +51,16 @@ public class Move {
 	
 	public void moveUp(int i) {
 		i = i-1;
-		x = boat.ploia.get(i).getX();
-		y = boat.ploia.get(i).getY();
-		size = boat.ploia.get(i).getSize();
-		direction = boat.ploia.get(i).getDirection();
+		x = Ship2.shipsList.get(i).getX();
+		y = Ship2.shipsList.get(i).getY();
+		size = Ship2.shipsList.get(i).getSize();
+		direction = Ship2.shipsList.get(i).getDirection();
 		
 		if (direction.equals("DOWN") ) {
 			if (x > 1) {
 				MenuInterface.Deck1.deck_arr[x+size-1][y] = "O";
 				MenuInterface.Deck1.deck_arr[x-1][y] = "S";
-				boat.ploia.get(i).setX(x-1);
+				Ship2.shipsList.get(i).setX(x-1);
 			} else {
 				System.out.println("Δεν επιτρέπεται!");
 			}
@@ -70,16 +70,16 @@ public class Move {
 	
 	public void moveDown(int i) {
 		i = i-1;
-		x = boat.ploia.get(i).getX();
-		y = boat.ploia.get(i).getY();
-		size = boat.ploia.get(i).getSize();
-		direction = boat.ploia.get(i).getDirection();
+		x = Ship2.shipsList.get(i).getX();
+		y = Ship2.shipsList.get(i).getY();
+		size = Ship2.shipsList.get(i).getSize();
+		direction = Ship2.shipsList.get(i).getDirection();
 		
 		if (direction.equals("DOWN") ) {
 			if (x + size - 1 <= 9) {
 				MenuInterface.Deck1.deck_arr[x][y] = "O";
 				MenuInterface.Deck1.deck_arr[x+size][y] = "S";
-				boat.ploia.get(i).setX(x+1);
+				Ship2.shipsList.get(i).setX(x+1);
 			} else {
 				System.out.println("Δεν επιτρέπεται!");
 			}
