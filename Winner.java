d7d3fd5f90848f2da1881d592a5a8dce8b31c511
demetres12 <93869307+demetres12@users.noreply.github.com>
2021-12-03 22:25:50 +0200

    Update Winner.java

--- Winner.java
+++ Winner.java
@@ -12,31 +12,34 @@ public class Winner implements ActionListener {
     Winner(int playerNo) {
         this.playerNo = playerNo;
         winnerframe = new JFrame("Results");
-        winnerpanel = new JPanel();
         winnerbutton = new JButton("Ok");
         winnerbutton.addActionListener(this);
+        winnerbutton.setBounds(125, 300, 200, 50);
 
 
         winnerlabel = new JLabel("The winner is player :  " );
-        winnerlabel.setFont(new Font("Serif", Font.PLAIN, 30));
+        winnerlabel.setFont(new Font("Serif", Font.BOLD, 30));
+        winnerlabel.setBounds(125,200,200,50);
+        winnerlabel.setLayout(null);
+
         winnerlabel2 = new JLabel();
         winnerlabel2.setText(String.valueOf(playerNo));
-        winnerlabel2.setFont(new Font("Serif", Font.PLAIN, 30));
+        winnerlabel2.setFont(new Font("Serif", Font.BOLD, 30));
+        winnerlabel2.setBounds(125,200,200,50);
+        winnerlabel2.setLayout(null);
+
+
+        winnerframe = new JFrame("Results");
 
         winnerframe.setSize(500,500 );
         winnerframe.setLayout(null);
         winnerframe.setContentPane(new JLabel(new ImageIcon("C:\\Users\\dimit\\Desktop\\orange.jpg")));
-
         winnerframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        winnerframe.setTitle("Results");
         winnerframe.setVisible(true);
 
         winnerframe.add(winnerbutton);
         winnerframe.add(winnerlabel);
         winnerframe.add(winnerlabel2);
-        winnerbutton.setBounds(125, 300, 200, 50);
-        winnerlabel.setBounds(125,200,200,50);
-        winnerlabel2.setBounds(125,200,200,50);
     }
 
 
