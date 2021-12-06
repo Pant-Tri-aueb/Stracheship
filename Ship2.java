b304c26e271b0691cc4ceebfe35880df43e4c3a6
GrammatikakisDimitris <92916988+GrammatikakisDimitris@users.noreply.github.com>
2021-12-06 13:21:13 +0200

    Update Ship2.java

--- Ship2.java
+++ Ship2.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,7 +6,9 @@ public class Ship2 {
 
     static ArrayList<Ship2> shipsList = new ArrayList<Ship2>(10);
 	
+    
 	public static int Shipcounter = 0;
+	
 	
 	private int ShipNumber;
     private int capacity;
@@ -13,6 +16,7 @@ public class Ship2 {
     private int x;
     private int y;
     private int size;
+    public int[][] xy = new int[4][2]; 
     private String direction;
 
    
@@ -22,13 +26,60 @@ public class Ship2 {
         this.size = size;
         this.direction = direction;
         this.ShipNumber = Shipcounter + 1;
-    
+        
+        this.xy[0][0] = x - 1;
+        this.xy[0][1] = y - 1;
+        if (this.direction.equals("DOWN")) {    
+        
+        	for(int i = 1; i < this.size; i++) {
+        	    
+        		this.xy[i][0] = (x - 1) + i;
+        	    this.xy[i][1] = y - 1; 
+        	}
+        
+        } else if (this.direction.equals("RIGHT")) {
+            
+        	for(int i = 1; i < this.size; i++) {
+        	    
+        		this.xy[i][0] = x - 1;
+        	    this.xy[i][1] = (y - 1) + i;
+        	}
+        }
     }
     
     public Ship2() {
     	
     }
 
+    public boolean isDestroyed1() {
+    	boolean result = true;
+    	
+    	for (int i = 0; i < this.size; i++) {
+    		
+    		if (MenuInterface.Deck1.deck_arr[xy[i][0]][xy[i][1]] == "S") {
+    			
+    			result = false;
+    		}
+    		
+    	}
+    	
+    	return result;
+    }
+
+    public boolean isDestroyed2() {
+	    boolean result = true;
+        
+	    for (int i = 0; i < this.size; i++) {
+	       
+	    	if (MenuInterface.Deck2.deck_arr[xy[i][0]][xy[i][1]] == "S") {
+              
+	        	result = false;
+            }
+        }
+        
+	    return result;
+    }
+    
     public int getCapacity() {
         return capacity;
     }
