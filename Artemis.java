ab8c7e717884c1fb7b731ec2e20d397b2dd0ff3c
GrammatikakisDimitris <92916988+GrammatikakisDimitris@users.noreply.github.com>
2021-12-10 12:50:47 +0200

    Update Artemis.java

--- Artemis.java
+++ Artemis.java
@@ -1,56 +1,99 @@
-import java.util.Scanner;
+ import java.awt.*;
+    import java.awt.event.ActionEvent;
+    import java.awt.event.ActionListener;
+    import javax.swing.*;
+    public class Artemis extends JFrame implements ActionListener {
 
-public class Artemis {
-
-    private static int[] RIVAL_MOVES = {2,2};
-    private boolean used = false;
-    private int playerNo;
-
-    public Artemis(int playerNo) {
-        this.playerNo = playerNo;
-    }
-
-    // another class interacts to use or not defence
-    public void askPlayer() {
-        boolean valid = true;
-        String validAnswer[] = {"Yes", "No"};
-        Scanner keyboard = new Scanner(System.in);
-        do {
-            System.out.println("Would you like to use your power?");
-            System.out.println("Answer with a YES or a NO.");
-            String answer = keyboard.nextLine();
-            outer : if (answer.equals(validAnswer[0])) {
-                if (RIVAL_MOVES[playerNo - 1] <= 0) {
-                    System.out.println("No more power available");
-                    break outer;
-                }
-                System.out.println("Ok. Power used. Available power for defence :" + --RIVAL_MOVES[playerNo - 1]);
-                used = true;
-            } else if (answer.equals(validAnswer[1])) {
-                System.out.println("Ok. No power used. Available power for defence :" + RIVAL_MOVES[playerNo - 1]);
-            } else {
-                valid = false;
-                System.out.println("Provide a valid answer");
-            }
-        } while (valid = false);
-    }
-
-    public void useDefence(Ship3 obj) { // the ship that uses the power
-        askPlayer();
-        if (used == true) {
-            int changeTolerance = obj.getTolerance();
-            obj.setTolerance(++changeTolerance);
+    	 public static int[] RIVAL_MOVES = {2,2};
+    	 static int x;
+    	
+    	    
+        JButton button;
+        JTextField textField;
+        JFrame frame;
+        
+        Artemis(){
         }
-    }
 
-    public void throwArrow(Ship3 obj, Deck deck) {
-        ArtemisArrow a = new ArtemisArrow();
-        /*if (deck.deck_arr[a.textField.getText() - 1][y - 1] == "S") {
-            obj.setTolerance(0);
-        } else if (deck.deck_arr[x - 1][y - 1] == "O") {
-            System.out.println("Didn't hit a ship");
+        public void insertData() {
+        	frame = new JFrame("Συντεταγμένες");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new FlowLayout());
+            button = new JButton("ΕΠΙΘΕΣΗ!");
+            button.addActionListener(this);
+            textField = new JTextField();
+            textField.setPreferredSize(new Dimension(250,40));
+            textField.setFont(new Font("Consolas",Font.BOLD, 25));
+
+
+            frame.add(button);
+            frame.add(textField);
+            frame.setVisible(true);
+            frame.pack();
 
         }
-    *//
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == button) {
+               String answer = textField.getText();
+               x = Integer.parseInt(answer);
+               frame.setVisible(false);
+               if (Game.gameState == 1) {
+            	   
+            	   throwArrow(MenuInterface.Deck2);
+            	   
+               } else {
+            	   
+            	   throwArrow(MenuInterface.Deck1);
+               }
+               
+             }
+        }
+        
+	    public static boolean capacity(int index) {
+	        boolean available = true;
+	    	
+	        if (RIVAL_MOVES[index] > 0) {
+	        	available = true;
+	        	RIVAL_MOVES[index]--;
+	        
+	        } else {
+	        	
+	        	available = false;
+	        }
+	        
+	        return available;        
+	            
+	       
+	     }
+
+	    public void useDefence(Ship2 obj,Deck deck) { // the ship that uses the power
+	       
+	    	  for (int i = 0; i < obj.getSize(); i++) {
+    			
+    			if (deck.deck_arr[obj.xy[i][0]][obj.xy[i][1]] == "X" ) {
+    				
+    				deck.deck_arr[obj.xy[i][0]][obj.xy[i][1]] = "S";
+    			}
+	       
+	    	   
+	       
+        }
+	        
+	        
+	    }
+
+	    public void throwArrow(Deck deck) {
+	       
+	       for (int y = 0; y < 10; y ++) {
+	        	 if (deck.deck_arr[x - 1][y] == "S") {
+	    	            
+	        		  deck.deck_arr[x - 1][y] = "X";
+	    	        	
+	        	 }
+	        }
+	      
+	    
+	    }
+
     }
-}
