bebb9c721356c184c262148e8692c98661a8430a
GrammatikakisDimitris <92916988+GrammatikakisDimitris@users.noreply.github.com>
2021-12-27 19:35:11 +0200

    Update GUIWINNER.java

--- GUIWINNER.java
+++ GUIWINNER.java
@@ -20,58 +20,11 @@ public class GUIWINNER {
         
         ImageIcon img = new ImageIcon("navmaxia.jpg"); 
         ImageIcon img2 = new ImageIcon("logo.jpg");
-
-        /*if (Deck1.checkWinner() == true ) {
-            JLabel label = new JLabel(String.format("The winner is player %d%s", Deck1.getWinnerName() , "Name")); // create
-            //label.setText("Winner is player: "); // set text for label
-            label.setIcon(img);
-            label.setHorizontalTextPosition(JLabel.CENTER); // set text left , center of imageicon
-            label.setVerticalTextPosition(JLabel.TOP); // SET TEXT TOP CENTER
-            label.setForeground(Color.WHITE); // set font color of text
-            label.setFont(new Font("MV Boli", Font.BOLD, 30)); // set font of text
-            label.setIconTextGap(0); // set gap fo text to image
-            label.setBackground(Color.BLACK);
-            label.setOpaque(true);
-    
-    
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(565,458);
-            frame.setVisible(true);
-            frame.setResizable(false);
-            frame.add(label);
-            frame.setIconImage(img2.getImage());
-            frame.pack();
-        } else if (Deck2.checkWinner() == true) {
-            JLabel label = new JLabel(String.format("The winner is player %d%s", Deck2.getWinnerName() , "Name")); // create
-            //label.setText("Winner is player: "); // set text for label
-            label.setIcon(img);
-            label.setHorizontalTextPosition(JLabel.CENTER); // set text left , center of imageicon
-            label.setVerticalTextPosition(JLabel.TOP); // SET TEXT TOP CENTER
-            label.setForeground(Color.WHITE); // set font color of text
-            label.setFont(new Font("MV Boli", Font.BOLD, 30)); // set font of text
-            label.setIconTextGap(0); // set gap fo text to image
-            label.setBackground(Color.BLACK);
-            label.setOpaque(true);
-    
-    
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(565,458);
-            frame.setVisible(true);
-            frame.setResizable(false);
-            frame.add(label);
-            frame.setIconImage(img2.getImage());
-            frame.pack();
-        } */
-
         
-
         JLabel label = new JLabel(String.format("The winner is %s", 
         		(MenuInterface.Deck1.checkWinner() == true) || (MenuInterface.Deck2.checkWinner() == true)  ?  
         				Deck.getWinnerName() : "There is no winner yet :(" )); // create
         //label.setText("Winner is player: "); // set text for label
-        
         
         
         label.setIcon(img);
